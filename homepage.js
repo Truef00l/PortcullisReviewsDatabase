3172fa8523c7988e2b740f4135f32e5631f55507
Iruledaworld1 <chava.vishruth@gmail.com>
2024-06-07 00:38:02 +0800

    Edited stuff

--- homepage.js
+++ homepage.js
@@ -1,1 +1,10 @@
 
+function openNav() {
+    document.getElementById("mySidebar").style.width = "200px";
+    document.getElementById("containerwith-openbutn").style.marginLeft = "200px";
+}
+  
+function closeNav() {
+    document.getElementById("mySidebar").style.width = "0";
+    document.getElementById("containerwith-openbutn").style.marginLeft= "0";
+}
