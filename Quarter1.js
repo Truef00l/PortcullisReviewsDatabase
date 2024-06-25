5865eda4972e4f2a289f3da7da69cb9d3771d605
Iruledaworld1 <chava.vishruth@gmail.com>
2024-06-25 23:44:23 +0800

    FUCKING HELL IM DONE

--- Quarter1.js
+++ Quarter1.js
@@ -40,10 +40,20 @@ function InsRow() {
   var cell2 = row.insertCell(1);
   var cell3 = row.insertCell(2);
   var cell4 = row.insertCell(3);
+  var cell5 = row.insertCell(4);
   cell1.innerHTML = "InvNew";
   cell2.innerHTML = "A/B/C/D";
   cell3.innerHTML = "Sigma";
   cell4.innerHTML = "X-X-XX";
+  cell5.innerHTML = 
+  
+  `<button onclick="InvPop()">Edit</button>
+    <div class="outer" style="clear: both;display: none;" id="InvPop">
+      <div class="inner">Foo foo
+        <button type="button" onclick="InvPopOp()"> X </button>
+      </div>
+    </div>
+`
 }
 
 function myDeleteFunction() {
@@ -56,3 +66,21 @@ $(window).on("load resize ", function() {
   var scrollWidth = $('.tbl-content').width() - $('.tbl-content table').width();
   $('.tbl-header').css({'padding-right':scrollWidth});
 }).resize();
+
+// popup when investors list is clicked //
+
+function InvPop() {
+  var x = document.getElementById("InvPop");
+  if (x.style.display === "none") {
+    x.style.display = "flex";
+  } else {
+    x.style.display = "none";
+  }
+  
+}
+
+function InvPopOp() {
+  var x = document.getElementById("InvPop");
+    x.style.display = "none";
+}
+  
